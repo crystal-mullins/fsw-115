222c57c864e09b66a0d29a9353d72ae0f319f214
Crystal Mulluns <stardustCrystal@users.noreply.github.com>
2020-06-06 18:45:05 -0700

    P

---
+++ week8/summery/summery.js
@@ -0,0 +1,16 @@
+// var summery = document.getElementById("summery")
+// var comment = document.getElementById("comment")
+
+// comment.textContent = summery.comment.value
+
+// comment.addEventListener(submit, function(e){
+//     e.preventDefault() 
+//     // var comment = document.getElementByClass("comment")
+//     if(comment = ""){
+//         alert("Thank you for your comment" + comment)
+// }})
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// const store = createStore(
+//     reducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   ); 
